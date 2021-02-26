7194579b4fa70fa1b6a0901d0eebb60e16288f62
Alejandro Rangel <alerancur@gmail.com>
2021-02-26 01:19:57 -0500

    Initial commit

---
+++ XUnitTest.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace XUnitTest.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ XUnitTest.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace XUnitTest.Model
+{
+    public class Class1
+    {
+    }
+}
